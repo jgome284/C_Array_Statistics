7c0397cd13e028bd514a7308c476c3de2ba857be
Jose <gomezjosem@live.com>
2022-09-27 13:46:54 -0400

    Added code, just need to add some more comments to clean it up and make it easy to follow.

--- stats.c
+++ stats.c
@@ -27,16 +27,25 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+int main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
-
+  // Sort Array to continue data processing.
+  sort_array(test, SIZE);
+  
+  
   /* Other Variable Declarations Go Here */
+  
+ 
   /* Statistics and Printing Functions Go Here */
+  print_statistics(test, SIZE);
+  print_array(test, SIZE);
+    
+  return 0;
 
 }
 
--- stats.h
+++ stats.h
@@ -35,6 +35,186 @@
  *
  * @return <Add Return Informaiton here>
  */
+ 
+double find_mean(unsigned char arr[], int size);
+
+double find_mean(unsigned char arr[], int size) {
+
+   int i;
+   double avg;
+   double sum = 0;
+
+   for (i = 0; i < size; ++i) {
+      sum += arr[i];
+   }
+
+   avg = sum / size;
+
+   return avg;
+}
+
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+double find_minimum(unsigned char arr[], int size);
+
+double find_minimum(unsigned char arr[], int size) {
+	
+	int location = 0;
+	int c;
+	
+	for (c = 1; c < size; c++){
+		if (arr[c] < arr[location]){
+			location = c;
+		}			
+	}
+	
+   return arr[location];
+}
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+double find_maximum(unsigned char arr[], int size);
+
+double find_maximum(unsigned char arr[], int size) {
+
+	int location = 0;
+	int c;
+	
+	for (c = 1; c < size; c++){
+		if (arr[c] > arr[location]){
+			location = c;
+		}			
+	}
+	
+   return arr[location];
+}
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+double find_median(unsigned char arr[], int size);
+
+double find_median(unsigned char arr[], int size) {
+
+    if (size % 2 == 0)
+    {
+        return (arr[(size - 1) / 2] + arr[size / 2]) / 2.0;
+    }
+    else
+    {
+        return arr[size / 2];
+    }
+
+}
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+ 
+double print_statistics(unsigned char arr[], int size);
+double print_statistics(unsigned char arr[], int size){
+	printf("The Array has a minimum value of %f\n", find_minimum(arr, size));
+	printf("The Array has a maximum value of %f\n", find_maximum(arr, size));
+	printf("The Array has a mean value of %f\n", find_mean(arr, size));
+	printf("The Array has a median value of %f\n", find_median(arr, size));
+	
+	return 0;
+	
+}
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * Given an array of data and a length, sorts the array from largest to
+ * smallest.  (The zeroth Element should be the largest value, and the 
+ * last element (n-1) should be the smallest value. )
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+void sort_array(unsigned char arr[], int size);
+void sort_array(unsigned char arr[], int size){
+   
+   int i, j, t;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+            }
+        }
+    }
+}
+
+void print_array(unsigned char arr[], int size);
+void print_array(unsigned char arr[], int size){
+	
+	printf("\nThe Array is made up of the following values:\n");
+    for (int i = 0; i < size; i++){
+		
+		printf("%u ",arr[i]);
+		
+		if (i%5 == 0){
+			
+			if (i == 0){
+			continue;	
+			}
+			
+			printf("\n");
+		}
+    }
+}
 
 
 #endif /* __STATS_H__ */
